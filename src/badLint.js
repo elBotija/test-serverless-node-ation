515687e8074a42680d2577f59ebca8ba90d50282
mauro <mauro@MacBook-Pro-de-mauro.local>
2024-12-27 11:26:10 -0300

    allPass

--- src/badLint.js
+++ src/badLint.js
@@ -1,10 +1,10 @@
-var foo = 'bar'    // var instead of const/let and missing semicolon
+const foo = 'bar'; // var instead of const/let and missing semicolon
 
-function badFunction(a,b) {    // no space after comma
-    return a+b                 // missing spaces around operator and semicolon
+function badFunction(a, b) { // no space after comma
+  return a + b; // missing spaces around operator and semicolon
 }
 
 // unused variables
-const unused = 'unused'
-
-export default badFunction
+const unused = 'unused';
+console.log(foo, unused);
+export default badFunction;
